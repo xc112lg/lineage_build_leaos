dc13d39409d4b71c23e32671548e4d9461c4da0a
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-04 01:10:38 +0800

    Update apply_patches.sh

--- apply_patches.sh
+++ apply_patches.sh
@@ -2,29 +2,40 @@
 
 set -e
 
-patches="$(readlink -f -- $1)"
+patches="$(readlink -f -- "$1")"
 
 shopt -s nullglob
-for project in $(cd $patches; echo *);do
-	p="$(tr _ / <<<$project |sed -e 's;platform/;;g')"
-	[ "$p" == build ] && p=build/make
-	[ "$p" == vendor/hardware/overlay ] && p=vendor/hardware_overlay
-	[ "$p" == vendor/partner/gms ] && p=vendor/partner_gms
-	pushd $p
-	git clean -fdx; git reset --hard
-	for patch in $patches/$project/*.patch;do
-		if git apply --check $patch;then
-			git am $patch
-		elif patch -f -p1 --dry-run < $patch > /dev/null;then
-			#This will fail
-			git am $patch || true
-			patch -f -p1 < $patch
-			git add -u
-			git am --continue
-		else
-			echo "Failed applying $patch"
-			exit 1
-		fi
-	done
-	popd
+failed_patches=() # Array to store failed patches
+for project in "$patches"/*; do
+    p="$(basename "$project" | tr _ / | sed -e 's;platform/;;g')"
+    [ "$p" == build ] && p=build/make
+    [ "$p" == vendor/hardware/overlay ] && p=vendor/hardware_overlay
+    [ "$p" == vendor/partner/gms ] && p=vendor/partner_gms
+    pushd "$p"
+    git clean -fdx; git reset --hard
+    for patch in "$patches"/$(basename "$project")/*.patch; do
+        if git apply --check "$patch"; then
+            git am "$patch"
+        else
+            echo "Reverting changes from $patch"
+            git reset --hard HEAD # Reset changes from previous patch
+            if patch -f -p1 --dry-run < "$patch" > /dev/null; then
+                patch -f -p1 < "$patch"
+                git add -u
+                git commit -m "Reverted changes from failed patch: $(basename "$patch")"
+            else
+                echo "Failed applying $patch"
+                failed_patches+=("$patch") # Store failed patch
+            fi
+        fi
+    done
+    popd
 done
+
+# Display failed patches
+if [ ${#failed_patches[@]} -gt 0 ]; then
+    echo "Failed to apply the following patches:"
+    for failed_patch in "${failed_patches[@]}"; do
+        echo "$failed_patch"
+    done
+fi
