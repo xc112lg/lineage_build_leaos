667063eb818c0150a1981867e2d61615458e5dda
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-07 00:54:27 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -128,7 +128,7 @@ build_treble() {
     case "${1}" in
         ("64BVS") TARGET=treble_arm64_bvS;;
         ("64BVZ") TARGET=treble_arm64_bvZ;;
-        ("64BVN") TARGET=treble_arm64_avN;;
+        ("64BVN") TARGET=treble_arm64_bvN;;
         (*) echo "Invalid target - exiting"; exit 1;;
     esac
     lunch ${TARGET}-userdebug
