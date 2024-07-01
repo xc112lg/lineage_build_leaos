0413201c6e18476f7bfc3e581eaf3c9dcdeb355f
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-07-01 11:24:40 +0800

    Update PixelPropsUtils.java

--- PixelPropsUtils.java
+++ PixelPropsUtils.java
@@ -402,13 +402,14 @@ public class PixelPropsUtils {
 
     private static void spoofBuildGms() {
         // Alter build parameters to avoid hardware attestation enforcement
-   	setPropValue("BRAND", "Xiaomi");
-            setPropValue("MANUFACTURER", "Xiaomi");
-            setPropValue("PRODUCT", "ferrari");
-            setPropValue("DEVICE", "ferrari");
-            setPropValue("FINGERPRINT", "Xiaomi/ferrari/ferrari:5.0.2/LRX22G/V8.0.2.0.LXIMIDG:user/release-keyss");
-            setPropValue("MODEL", "Mi 4i");
-	        setVersionFieldString("SECURITY_PATCH", "2016-09-01");
+        setPropValue("BRAND", "google");
+        setPropValue("MANUFACTURER", "Google");
+        setPropValue("DEVICE", "sailfish");
+        setPropValue("ID", "OPM1.171019.011");
+        setPropValue("FINGERPRINT", "google/sailfish/sailfish:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+        setPropValue("MODEL", "Pixel");
+        setPropValue("PRODUCT", "sailfish");
+        setVersionFieldString("SECURITY_PATCH", "2017-12-05");
     }
 
     private static boolean isCallerSafetyNet() {
