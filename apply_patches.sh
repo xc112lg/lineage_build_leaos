45d1ba4513416a1335b36765291bc6440f0826af
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-08-30 11:27:52 +0800

    Update apply_patches.sh

--- apply_patches.sh
+++ apply_patches.sh
@@ -1,3 +1,53 @@
+# #!/bin/bash
+
+# set -e
+
+# patches="$(readlink -f -- "$1")"
+
+# shopt -s nullglob
+# failed_patches=() # Array to store failed patches
+# for project in "$patches"/*; do
+#     p="$(basename "$project" | tr _ / | sed -e 's;platform/;;g')"
+#     [ "$p" == build ] && p=build/make
+#     [ "$p" == vendor/hardware/overlay ] && p=vendor/hardware_overlay
+#     [ "$p" == vendor/partner/gms ] && p=vendor/partner_gms
+#     [ "$p" == external/harfbuzz/ng ] && p=external/harfbuzz_ng
+#     pushd "$p"
+#     git clean -fdx; git reset --hard
+#     for patch in "$patches"/$(basename "$project")/*.patch; do
+#         if git apply --check "$patch"; then
+#             git am "$patch"
+#             # Remove .orig files after successful patch application
+#             find . -name '*.orig' -delete
+#         else
+#             echo "Reverting changes from $patch"
+#             git reset --hard HEAD # Reset changes from previous patch
+#             if patch -f -p1 --dry-run < "$patch" > /dev/null; then
+#                 patch -f -p1 < "$patch"
+#                 git add -u
+#                 git commit -m "Reverted changes from failed patch: $(basename "$patch")"
+#                 # Remove .orig files after successful revert
+#                 find . -name '*.orig' -delete
+#             else
+#                 echo "Failed applying $patch"
+#                 failed_patches+=("$patch") # Store failed patch
+#             fi
+#         fi
+#     done
+#     popd
+# done
+
+# # Display failed patches
+# if [ ${#failed_patches[@]} -gt 0 ]; then
+#     echo "Failed to apply the following patches:"
+#     for failed_patch in "${failed_patches[@]}"; do
+#         echo "$failed_patch"
+#     done
+# fi
+
+
+
+
 #!/bin/bash
 
 set -e
@@ -43,4 +93,16 @@ if [ ${#failed_patches[@]} -gt 0 ]; then
     for failed_patch in "${failed_patches[@]}"; do
         echo "$failed_patch"
     done
+    
+    # Save the failed patches to a text file
+    failed_file="failed_patches.txt"
+    printf "%s\n" "${failed_patches[@]}" > "$failed_file"
+    echo "Saved failed patches to $failed_file"
 fi
+
+
+
+
+
+
+
